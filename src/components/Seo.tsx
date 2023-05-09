ab7102776335a99bb2f47e810e7e303eceac6025
권가은 <aileen1007@ewhain.net>
2023-05-10 03:52:36 +0900

    Add: 프로젝트 초기 세팅

---
+++ src/components/Seo.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+const Seo = ({ title }: { title: string }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+    </Head>
+  );
+};
+export default Seo;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+// 렌딩 페이지
+
+const Landing = () => {
+  return <div>This is Landing page.</div>;
+};
+
+export default Landing;
