8a1b9cce7f4d071708a8524b9d847cc4eeeb20c8
flowersayo <flowersayo0811@ewhain.net>
2023-05-11 00:11:34 +0900

    feat: splash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
 // 렌딩 페이지
+import Image from 'next/image';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 const Landing = () => {
-  return <div>This is Landing page.</div>;
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/home');
+    }, 3000);
+  }, []);
+
+  return (
+    <div className="container">
+      <Image
+        src="/images/netflix-logo.gif"
+        width={700}
+        height={500}
+        alt="netflix-logo"
+      />
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+        }
+      `}</style>
+    </div>
+  );
 };
 
 export default Landing;
