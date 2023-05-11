1629fd876b5e9a966e16c57bf3e6307301b26378
flowersayo <flowersayo0811@ewhain.net>
2023-05-12 03:51:30 +0900

    feat: fetch movie data ( SSR )

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,66 @@
 // 메인 페이지
+import FlatList from '../../components/FlatList';
+import { useState } from 'react';
+import { WEB_SERVER_URL } from '@/utils/constants';
 
-const Home = () => {
+const Home = ({
+  nowPlayingMovies,
+  popularMovies,
+  topRatedMovies,
+  upcomingMovies,
+}: any) => {
+  const sections = [
+    {
+      title: 'Now Playing',
+      data: nowPlayingMovies,
+    },
+    {
+      title: 'Top Rated',
+      data: topRatedMovies,
+    },
+    {
+      title: 'Popular',
+      data: popularMovies,
+    },
+    {
+      title: 'Upcoming',
+      data: upcomingMovies,
+    },
+  ];
   return (
     <div className="container">
-      This is Home page.
       <style jsx>{`
         .container {
           background-color: black;
-          width: 100%;
+          width: 400px;
+          overflow: hidden;
           height: 100%;
         }
       `}</style>
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const { results: nowPlayingMovies } = await (
+    await fetch(`${WEB_SERVER_URL}/api/movies/now_playing`)
+  ).json();
+
+  const { results: popularMovies } = await (
+    await fetch(`${WEB_SERVER_URL}/api/movies/popular`)
+  ).json();
+
+  const { results: topRatedMovies } = await (
+    await fetch(`${WEB_SERVER_URL}/api/movies/top_rated`)
+  ).json();
+
+  const { results: upcomingMovies } = await (
+    await fetch(`${WEB_SERVER_URL}/api/movies/upcoming`)
+  ).json();
+
+  return {
+    props: { nowPlayingMovies, popularMovies, topRatedMovies, upcomingMovies },
+  };
+}
 
 export default Home;
