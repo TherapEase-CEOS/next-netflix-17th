37e2ceed8d621339a8756eeed8a79c9c965f32b4
권가은 <aileen1007@ewhain.net>
2023-05-12 21:45:26 +0900

    Fix: localhost -> 127.0.0.1

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 export const fetchTrendingMovies = async () => {
-  const res = await fetch(`http://localhost:3000/api/movies/trending`);
+  const res = await fetch(`http://127.0.0.1:3000/api/movies/trending`);
   const data = await res.json();
 
   return data;
