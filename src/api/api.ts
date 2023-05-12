c47ddb7dffed0856b6780bcfe8f45e772081e66d
김서연 <86418674+flowersayo@users.noreply.github.com>
2023-05-13 00:44:34 +0900

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 export const fetchTrendingMovies = async () => {
-  const res = await fetch(`http://127.0.0.1:3000/api/movies/trending`);
+    const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`);
   const data = await res.json();
 
   return data;
